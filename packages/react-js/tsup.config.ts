b3fa7e92ecdc762a0a0b87e0889e63a2663dba48
jeff-wursta <jeff@wursta.com>
2024-10-15 12:59:51 -0400

    chore(sapp-46): add siwys button

--- packages/react-js/tsup.config.ts
+++ packages/react-js/tsup.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import fsPromises from "fs/promises";
 import postcss from "postcss";
 import postcssModules from "postcss-modules";
+import { generateScopedName } from "hash-css-selector";
 
 export default defineConfig({
   entry: ["src/index.ts"], // Entry point of the library
@@ -11,6 +12,9 @@ export default defineConfig({
   sourcemap: true, // Generate sourcemaps for debugging
   clean: true, // Clean the dist folder before building
   external: ["react", "react-dom"], // Mark peer dependencies as external
+  loader: {
+    ".css": "local-css",
+  },
   esbuildPlugins: [
     // see: https://github.com/egoist/tsup/issues/536#issuecomment-1302012400
     {
@@ -18,13 +22,15 @@ export default defineConfig({
       setup(build): void {
         build.onResolve(
           { filter: /\.module\.css$/, namespace: "file" },
-          (args) => ({
-            path: `${args.path}#css-module`,
-            namespace: "css-module",
-            pluginData: {
-              pathDir: path.join(args.resolveDir, args.path),
-            },
-          })
+          (args) => {
+            return {
+              path: `${args.path}#css-module`,
+              namespace: "css-module",
+              pluginData: {
+                pathDir: path.join(args.resolveDir, args.path),
+              },
+            };
+          }
         );
         build.onLoad(
           { filter: /#css-module$/, namespace: "css-module" },
@@ -38,12 +44,17 @@ export default defineConfig({
               "utf8"
             );
 
-            let cssModule = {};
+            let cssModule: any = {};
             const result = await postcss([
               postcssModules({
-                getJSON(_, json) {
-                  cssModule = json;
+                generateScopedName: function (name, filename) {
+                  const newSelector = generateScopedName(name, filename);
+                  cssModule[name] = newSelector;
+
+                  return newSelector;
                 },
+                getJSON: () => {},
+                scopeBehaviour: "local",
               }),
             ]).process(source, { from: pluginData.pathDir });
 

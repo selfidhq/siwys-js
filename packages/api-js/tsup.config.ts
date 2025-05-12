c8d58452246c707d5a3409fde835ce20a9cb295b
Facu Rossi <facundo.rossi@selfid.com>
2025-05-12 12:34:29 -0300

    feat(SAPP-221): updating Keymaster class to support mobile integration

--- packages/api-js/tsup.config.ts
+++ packages/api-js/tsup.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "tsup";
-
 export default defineConfig({
-  entry: ["src/index.ts"], // Entry point of the library
-  format: ["cjs", "esm"], // Output both ESM and CJS formats
+  entry: ["./src/index.ts"], // Archivos de entrada
+  format: ["esm", "cjs"], // Generar ambos formatos
+  target: "esnext", // Objetivo de compilación (puedes ajustarlo a tus necesidades)
+  sourcemap: true, // Generar mapas de fuente
+  clean: true, // Limpiar el directorio de salida antes de la compilación
   dts: {
     entry: "src/index.ts",
     resolve: true, // Generate TypeScript declaration files
-  },
-  sourcemap: true, // Generate sourcemaps for debugging
-  clean: true, // Clean the dist folder before building
-  external: ["react", "react-dom"], // Mark peer dependencies as external
+  }, // Generar archivos de declaración .d.ts
+  outDir: "dist",
+  external: ["react", "react-dom"], // Directorio de salida
 });
